605f62294eab35e65fb7f5276483585b1d8cc2de
Jedtapat Tanthiew <6410685090@cn351>
2024-04-17 19:21:38 +0700

    create tab sample

--- MyApp/App.jsx
+++ MyApp/App.jsx
@@ -1,15 +1,17 @@
 import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { onAuthStateChanged } from 'firebase/auth';
 import { firebase_auth } from './firebaseConfig';
 import Login from './app/screens/Login';
 import Register from './app/screens/Register';
 import Home from './app/screens/Home';
+import Test from './app/screens/Test';
 
 const Stack = createNativeStackNavigator();
-
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   const [user, setUser] = useState(null);
@@ -23,16 +25,50 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Navigator>
         {user ? (
-          <Stack.Screen name='Home' component={Home} />
+          <Stack.Screen
+            name="Authenticated"
+            component={AuthenticatedScreens}
+            options={{ headerShown: false }}
+          />
         ) : (
           <>
-            <Stack.Screen name="Login" component={Login} options={{headerShown: false}} />
-            <Stack.Screen name="Register" component={Register} options={{headerShown: false}} />
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Register"
+              component={Register}
+              options={{ headerShown: false }}
+            />
           </>
         )}
       </Stack.Navigator>
-  </NavigationContainer>
+      <StatusBar style="auto" />
+    </NavigationContainer>
   );
 }
+
+// Add more tap here
+function AuthenticatedScreens() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: 'blue',
+        tabBarInactiveTintColor: 'gray',
+        tabBarStyle: {
+          display: 'flex',
+        },
+      }}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Test" component={Test} />
+    </Tab.Navigator>
+
+  );
+}
+
+
--- MyApp/app/screens/Home.jsx
+++ MyApp/app/screens/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text , Button } from "react-native";
 import { firebase_auth } from "../../firebaseConfig";
+import Posts from "../components/Posts";
 
 const Home = () => {
     const handleLogout = async () => {
@@ -17,6 +18,7 @@ const Home = () => {
     return (
         <View>
             <Text>Home</Text>
+            <Posts />
             <Button title="Logout" onPress={handleLogout} />
         </View>
     );
