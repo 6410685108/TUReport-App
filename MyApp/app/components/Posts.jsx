ce169af24a29932746073cbc9e36de90e8580ecf
Techin Bunphuttharak <techin@Techins-MacBook-Pro.local>
2024-04-20 15:41:24 +0700

    changeHome

--- MyApp/app/components/Posts.jsx
+++ MyApp/app/components/Posts.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 
 const Posts = () => {
-    return (
-        <View>
-            <Text>Posts</Text>
-        </View>
-    );
-}
+  return (
+    <View>
+      <View>
+        <Image style={{width: 30, height: 30}}source={{uri: 'https://images.rawpixel.com/image_png_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIzLTAxL3JtNjA5LXNvbGlkaWNvbi13LTAwMi1wLnBuZw.png'}}/>
+        <Text>
+          Title: Title{"\n"}
+          Username: Username{"\n"}
+          Repost: 1{"\n"}
+          Status: Ok{"\n"}
+          Location: Map{"\n"}
+          Anonymous: Anonymous
+        </Text>
+      </View>
+    </View>
+  );
+};
 
 export default Posts;
