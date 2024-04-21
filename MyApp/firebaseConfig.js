65b8f6c61fcf1f2c623def75853e2936e0331b96
Jedtapat Tanthiew <6410685090@cn351>
2024-04-22 01:08:42 +0700

    delete notuse

--- MyApp/firebaseConfig.js
+++ MyApp/firebaseConfig.js
@@ -5,14 +5,14 @@ import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCGyBzOxbZmNRBFHdUwUmFJajPLQedmcAM",
-    authDomain: "tu-reports.firebaseapp.com",
-    databaseURL: "https://tu-reports-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "tu-reports",
-    storageBucket: "tu-reports.appspot.com",
-    messagingSenderId: "876108855213",
-    appId: "1:876108855213:web:502c7441495744de5f1324",
-    measurementId: "G-PPN4G1WE6G"
+  apiKey: "AIzaSyCGyBzOxbZmNRBFHdUwUmFJajPLQedmcAM",
+  authDomain: "tu-reports.firebaseapp.com",
+  databaseURL: "https://tu-reports-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "tu-reports",
+  storageBucket: "tu-reports.appspot.com",
+  messagingSenderId: "876108855213",
+  appId: "1:876108855213:web:502c7441495744de5f1324",
+  measurementId: "G-PPN4G1WE6G"
 };
 
 export const firebase_app = initializeApp(firebaseConfig);
