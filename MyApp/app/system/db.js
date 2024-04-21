694eb537363a7455c18fc84a0b80607d69d82397
Jedtapat Tanthiew <6410685090@cn351>
2024-04-21 23:01:04 +0700

    some func

--- MyApp/app/system/db.js
+++ MyApp/app/system/db.js
@@ -1,7 +1,7 @@
 import { firebase_db } from '../../firebaseConfig'; // Import firebase_db from your firebaseConfig
-import { collection , addDoc , getDocs } from 'firebase/firestore/lite';
+import { collection , addDoc , getDocs , updateDoc , doc , getDoc , setDoc } from 'firebase/firestore/lite';
 
-const collectPost = async (title, content, picUrl) => {
+const createPost = async (title, content, picUrl) => {
 
     try {
         const postCollectionRef = collection(firebase_db, 'posts');
@@ -9,11 +9,29 @@ const collectPost = async (title, content, picUrl) => {
             title: title,
             content: content,
             picUrl: picUrl,
+            
         });
     } catch (error) {
         console.error('Error creating post:', error);
     }
 
+};
+
+const editPost = async (postId, newTitle, newContent, newPicUrl) => {
+    const postCollectionRef = collection(firebase_db, 'posts');
+    const postDocRef = doc(postCollectionRef, postId);
+
+    const postDocSnap = await getDoc(postDocRef);
+
+    if (postDocSnap.exists()) {
+        await updateDoc(postDocRef, {
+            title: newTitle,
+            content: newContent,
+            picUrl: newPicUrl,
+        });
+    } else {
+        console.log('Post not found');
+    }
 };
 
 const getAllPosts = async () => {
@@ -28,9 +46,40 @@ const getAllPosts = async () => {
     }
 };
 
-const db = { 
-    collectPost , // done : pass # Not have comment now
+const createComment = async (postId, content, author) => {
+    try {
+        const commentCollectionRef = collection(firebase_db, 'posts', postId, 'comments');
+        await addDoc(commentCollectionRef, {
+            content: content,
+            author , author,
+        });
+    } catch (error) {
+        console.error('Error creating comment:', error);
+    }
+};
+
+
+const userBookmark = async (postId, userId) => {
+    try {
+        const userRef = doc(firebase_db, 'users', userId); 
+        const postCollectionRef = collection(userRef, 'postids'); 
+        await addDoc(postCollectionRef, {
+            postId: postId,
+        });
+    } catch (error) {
+        console.error('Error adding post ID:', error);
+    }
+};
+
+
+const db = {  // code : test
+    createPost , // done : pass # Not have comment now
+    editPost , // done : none
     getAllPosts , // done : pass
+
+    createComment , // done : none
+
+    userBookmark , // done : none
 
 };
 
