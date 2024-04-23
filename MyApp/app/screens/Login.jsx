123ac8e0a863f70985025f6f66b9edc7634a73f6
6410685199 <6410685199@student.tu.ac.th>
2024-04-23 21:33:52 +0700

    test gradient

--- MyApp/app/screens/Login.jsx
+++ MyApp/app/screens/Login.jsx
@@ -1,87 +1,183 @@
-import React , { useState } from "react";
-import { View, Text, StyleSheet , TextInput, ActivityIndicator, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  ActivityIndicator,
+  TouchableOpacity,
+  Dimensions,
+  Image,
+} from "react-native";
 import { firebase_auth } from "../../firebaseConfig";
 import { signInWithEmailAndPassword } from "firebase/auth";
+import { LinearGradient } from "expo-linear-gradient";
 
 const Login = ({ navigation }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [loading, setLoading] = useState(false);
-    const auth = firebase_auth;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const auth = firebase_auth;
 
-    const signIn = async () => {
-        setLoading(true);
-        try {
-            const response = await signInWithEmailAndPassword(auth,email, password);
-        } catch (error) {
-            alert("Invalid Email or Password");
-            console.log(error);
-        } finally {
-            setLoading(false);
-        }
+  const signIn = async () => {
+    setLoading(true);
+    try {
+      const response = await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      alert(error.message);
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    const easyLogin = async () => {
-        setLoading(true);
-        try {
-            const response = await signInWithEmailAndPassword(auth,"t@t.com", "111111");
-        } catch (error) {
-            alert(error.message);
-            console.log(error);
-        } finally {
-            setLoading(false);
-        }
+  const easyLogin = async () => {
+    setLoading(true);
+    try {
+      const response = await signInWithEmailAndPassword(
+        auth,
+        "t@t.com",
+        "111111"
+      );
+    } catch (error) {
+      alert(error.message);
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    return (
-        <View style={styles.container}>
-            <Text>Login Screen</Text>
-            <TextInput 
-                value={email} 
-                style={styles.input} 
-                placeholder="Email" 
-                autoCapitalize="none" 
-                onChangeText={(text) => setEmail(text)}
-            />
-            <TextInput 
-                secureTextEntry={true}
-                value={password} 
-                style={styles.input} 
-                placeholder="Password" 
-                autoCapitalize="none" 
-                onChangeText={(text) => setPassword(text)}
-            />
-            {loading ? (
-                <ActivityIndicator size="large" color="#0000ff" /> 
-            ) : (
-                <>
-                    <Button title="Login" onPress={signIn} />
-                    <Button title="Goto Register" onPress={() => navigation.navigate('Register')} />
-                    <Button title="Test Page For Backend" onPress={() => navigation.navigate('Test')} />
-                    <Button title="Test Page For Jsx" onPress={() => navigation.navigate('TestJsx')} />
-                    <Button title="EZ Login" onPress={easyLogin} />
-                </>
-            )}
-            
+  return (
+    <LinearGradient
+      // Gradient colors
+      colors={["#ff7100", "#ffda27"]}
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <View style={styles.titleContainer}>
+          <Image
+            source={require("../picture/logo.png")}
+            style={{
+              width: 200,
+              height: 200,
+              marginTop: 60,
+              marginBottom: -20,
+            }}
+          />
         </View>
+        <TextInput
+          value={email}
+          style={styles.input}
+          placeholder="Email"
+          autoCapitalize="none"
+          onChangeText={(text) => setEmail(text)}
+          placeholderTextColor="white"
+        />
 
-    );
-}
+        <TextInput
+          value={password}
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+          onChangeText={(text) => setPassword(text)}
+          placeholderTextColor="white"
+        />
+        {loading ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : (
+          <>
+            <TouchableOpacity style={styles.button} onPress={signIn}>
+              <Text style={styles.buttonText}>Sign In</Text>
+            </TouchableOpacity>
+            <View style={styles.lineContainer}>
+              <View style={styles.line}></View>
+              <Text style={styles.orText}>or</Text>
+              <View style={styles.line}></View>
+            </View>
+            <Text style={{ color: "white", marginTop: -5 }}>Continue with</Text>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => navigation.navigate("Register")}
+            >
+              <Text style={styles.buttonText}>Go to Register</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => navigation.navigate("Test")}
+            >
+              <Text style={styles.buttonText}>Test Page For Backend</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.button} onPress={easyLogin}>
+              <Text style={styles.buttonText}>EZ Login</Text>
+            </TouchableOpacity>
+          </>
+        )}
+      </View>
+    </LinearGradient>
+  );
+};
+
+const { width, height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
-    container: {
-       marginHorizontal: 20,
-       flex: 1,
-       justifyContent: 'center',
-    },
-    input: {
-        marginVertical: 4,
-        height: 50,
-        borderWidth: 1,
-        borderRadius: 4,
-        padding: 10,
-        backgroundColor: '#fff',
-    }
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  titleContainer: {
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  input: {
+    marginVertical: 4,
+    height: 50,
+    width: width * 0.75,
+    borderWidth: 3,
+    borderRadius: 25,
+    padding: 10,
+    borderColor: "white",
+    color: "white",
+    textAlign: "center",
+  },
+  button: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    marginVertical: 10,
+    width: width * 0.5,
+    borderRadius: 25,
+  },
+  lineContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  line: {
+    flex: 1,
+    height: 2,
+    backgroundColor: "white",
+    marginHorizontal: 5,
+    width: width * 0.5,
+  },
+  orText: {
+    color: "white",
+    marginHorizontal: 5,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "black",
+    fontWeight: "700",
+  },
+  background: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0,
+    height: Dimensions.get("window").height,
+  },
 });
 
 export default Login;
--- MyApp/app/screens/Register.jsx
+++ MyApp/app/screens/Register.jsx
@@ -1,78 +1,113 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, ActivityIndicator } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  ActivityIndicator,
+} from "react-native";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { firebase_auth } from "../../firebaseConfig";
+import { LinearGradient } from "expo-linear-gradient";
 
 const RegisterScreen = ({ navigation }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [loading, setLoading] = useState(false);
-    const auth = firebase_auth;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const auth = firebase_auth;
 
-    const handleRegister = async () => {
-        setLoading(true);
-        try {
-            const response = await createUserWithEmailAndPassword(auth,email, password);
-        } catch (error) {
-            alert(error.message);
-            console.log(error);
-        } finally {
-            setLoading(false);
-        }
+  const handleRegister = async () => {
+    setLoading(true);
+    try {
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+    } catch (error) {
+      alert(error.message);
+      console.log(error);
+    } finally {
+      setLoading(false);
     }
+  };
 
   return (
-    <View style={styles.container}>
-      <Text>Register Screen</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Confirm Password"
-        value={confirmPassword}
-        onChangeText={setConfirmPassword}
-        secureTextEntry
-      />
-      {loading ? (
-                <ActivityIndicator size="large" color="#0000ff" /> 
-            ) : (
-                <>
-                    <Button title="Register" onPress={handleRegister} />
-                    <Button title="Goto Login" onPress={() => navigation.navigate('Login')} />
-                </>
-            )}
-     
-    </View>
+    <LinearGradient
+      // Gradient colors
+      colors={["#ff7100", "#ffda27"]}
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <Text style={styles.header}>Create Account</Text>
+        <Text style={{ color: "white", marginTop: -5 }}>Email</Text>
+        <TextInput
+          style={styles.input}
+          placeholder=""
+          value={email}
+          onChangeText={setEmail}
+        />
+        <Text style={{ color: "white", marginTop: -5 }}>Password</Text>
+        <TextInput
+          style={styles.input}
+          placeholder=""
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        <Text style={{ color: "white", marginTop: -5 }}>Comfirm Password</Text>
+        <TextInput
+          style={styles.input}
+          placeholder=""
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+          secureTextEntry
+        />
+        {loading ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : (
+          <>
+            <Button title="Register" onPress={handleRegister} />
+            <Button
+              title="Go to Login"
+              onPress={() => navigation.navigate("Login")}
+            />
+          </>
+        )}
+      </View>
+    </LinearGradient>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-       marginHorizontal: 20,
-       flex: 1,
-       justifyContent: 'center',
-    },
-    input: {
-        marginVertical: 4,
-        height: 50,
-        borderWidth: 1,
-        borderRadius: 4,
-        padding: 10,
-        backgroundColor: '#fff',
-    }
+  container: {
+    marginHorizontal: 20,
+    flex: 1,
+    justifyContent: "center",
+  },
+  input: {
+    marginVertical: 4,
+    height: 50,
+    borderWidth: 3,
+    borderRadius: 25,
+    padding: 10,
+    borderColor: "white",
+    color: "white",
+    textAlign: "center",
+  },
+  header: {
+    textAlign: "center",
+    marginBottom: 20,
+    fontSize: 24,
+    color: "#fff",
+  },
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
-
 
 export default RegisterScreen;
