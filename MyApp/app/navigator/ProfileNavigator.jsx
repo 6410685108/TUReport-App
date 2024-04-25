294949645767ea5a6489f9969ef27e12c72b78d4
Jedtapat Tanthiew <6410685090@cn351>
2024-04-25 16:19:14 +0700

    Profile Navigator

---
+++ MyApp/app/navigator/ProfileNavigator.jsx
@@ -0,0 +1,28 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Posts from '../components/Posts';
+import Profile from '../tabs/Profile';
+import Inpost from '../screens/Inpost';
+
+const Stack = createNativeStackNavigator();
+
+export default function ProfileNavigator() {
+    return (
+            <Stack.Navigator initialRouteName='Profile2'>
+                <Stack.Screen
+                    name="Profile2"
+                    component={Profile}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="Inpost"
+                    component={Inpost}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="Posts"
+                    component={Posts}
+                    options={{ headerShown: false }}
+                />
+            </Stack.Navigator>
+    );
+  }
