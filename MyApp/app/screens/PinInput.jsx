221a7b5148a77507fc79eab0c9cc114b1fb8ef06
6410685199 <6410685199@student.tu.ac.th>
2024-04-27 21:22:06 +0700

    adjust contact admin

--- MyApp/app/screens/PinInput.jsx
+++ MyApp/app/screens/PinInput.jsx
@@ -1,149 +1,157 @@
-import React, { useState , useContext ,useEffect,useRef} from 'react';
-import Icon from "react-native-vector-icons/Ionicons"
-import { View, TextInput, Button ,StyleSheet, Image, Text, TouchableOpacity, Alert , Keyboard , SafeAreaView, StatusBar} from 'react-native';
-import { db } from '../system/db';
-import * as ImagePicker from 'expo-image-picker';
-import { SettingContext } from '../system/setting';
-import PinView from 'react-native-pin-view';
-import ReactNativePinView from "react-native-pin-view"
+import React, { useState, useContext, useEffect, useRef } from "react";
+import Icon from "react-native-vector-icons/Ionicons";
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  Image,
+  Text,
+  TouchableOpacity,
+  Alert,
+  Keyboard,
+  SafeAreaView,
+  StatusBar,
+} from "react-native";
+import { db } from "../system/db";
+import * as ImagePicker from "expo-image-picker";
+import { SettingContext } from "../system/setting";
+// import PinView from 'react-native-pin-view';
+// import ReactNativePinView from "react-native-pin-view";
 
 const Pin = () => {
+  const { setting } = useContext(SettingContext);
+  const { theme, language } = setting;
+  const styles = theme === "light" ? lightstyles : darkstyles;
 
-    const { setting } = useContext(SettingContext);
-    const { theme, language } = setting;
-    const styles = theme === 'light' ? lightstyles : darkstyles;
+  const [enteredPin, setEnteredPin] = useState("");
+  const pinViewRef = useRef(null);
 
-    const [enteredPin, setEnteredPin] = useState('');
-    const pinViewRef = useRef(null);
-  
-    const handlePinChange = (value) => {
-      setEnteredPin(value);
-    };
-  
-    const handleCustomLeftButtonPress = () => {
-      if (enteredPin.length > 0) {
-        setEnteredPin(enteredPin.slice(0, -1));
-      }
-    };
-  
-    const handleCustomRightButtonPress = () => {
-      if (enteredPin.length === 6) {
-        alert(`Entered PIN: ${enteredPin}`);
-      } else {
-        alert('Please enter a 6-digit PIN');
-      }
-    };
-  
-    return (
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.pinText}>PIN</Text>
-        <View style={styles.pinViewContainer}>
-          <ReactNativePinView
-            ref={pinViewRef}
-            pinLength={6} // Enforce a 6-digit PIN
-            onValueChange={handlePinChange}
-            inputViewEmptyStyle={styles.inputViewEmptyStyle}
-            inputViewFilledStyle={styles.inputViewFilledStyle}
-            buttonViewStyle={styles.buttonViewStyle}
-            buttonTextStyle={styles.buttonTextStyle}
-            customLeftButton={
-              <View>
-                {/* Conditionally display "Backspace" button */}
-                {enteredPin.length > 0 && (
-                  <Icon name="backspace" size={36} color="gray" />
-                )}
-              </View>
+  const handlePinChange = (value) => {
+    setEnteredPin(value);
+  };
+
+  const handleCustomLeftButtonPress = () => {
+    if (enteredPin.length > 0) {
+      setEnteredPin(enteredPin.slice(0, -1));
+    }
+  };
+
+  const handleCustomRightButtonPress = () => {
+    if (enteredPin.length === 6) {
+      alert(`Entered PIN: ${enteredPin}`);
+    } else {
+      alert("Please enter a 6-digit PIN");
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.pinText}>PIN</Text>
+      <View style={styles.pinViewContainer}>
+        <ReactNativePinView
+          ref={pinViewRef}
+          pinLength={6} // Enforce a 6-digit PIN
+          onValueChange={handlePinChange}
+          inputViewEmptyStyle={styles.inputViewEmptyStyle}
+          inputViewFilledStyle={styles.inputViewFilledStyle}
+          buttonViewStyle={styles.buttonViewStyle}
+          buttonTextStyle={styles.buttonTextStyle}
+          customLeftButton={
+            <View>
+              {/* Conditionally display "Backspace" button */}
+              {enteredPin.length > 0 && (
+                <Icon name="backspace" size={36} color="gray" />
+              )}
+            </View>
+          }
+          customRightButton={
+            <View>
+              {/* Only display "Unlock" button when PIN length is 6 */}
+              {enteredPin.length === 6 && (
+                <Icon name="lock-open" size={36} color="gray" />
+              )}
+            </View>
+          }
+          onButtonPress={(key) => {
+            if (key === "custom_left") {
+              handleCustomLeftButtonPress();
+            } else if (key === "custom_right") {
+              handleCustomRightButtonPress();
             }
-            customRightButton={
-              <View>
-                {/* Only display "Unlock" button when PIN length is 6 */}
-                {enteredPin.length === 6 && (
-                  <Icon name="lock-open" size={36} color="gray" />
-                )}
-              </View>
-            }
-            onButtonPress={(key) => {
-              if (key === 'custom_left') {
-                handleCustomLeftButtonPress();
-              } else if (key === 'custom_right') {
-                handleCustomRightButtonPress();
-              }
-            }}
-          />
-        </View>
-      </SafeAreaView>
-    );
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  );
 };
 
-
 const lightstyles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#FFF',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: "100%",
-      },
-      pinText: {
-        color: 'black',
-        fontSize: 48,
-        paddingBottom: 48,
-      },
-      pinViewContainer: {
-        marginTop: 24,
-      },
-      inputViewEmptyStyle:{
-        borderWidth: 1, 
-        borderColor: 'black', 
-        backgroundColor: 'white',
-        margin:12   
-    },
-    inputViewFilledStyle:{
-        backgroundColor: 'black', 
-    },
-    buttonViewStyle:{
-        borderWidth: 1, 
-        borderColor: 'black',
-    },
-    buttonTextStyle:{
-        color: 'black',
-    }
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+  },
+  pinText: {
+    color: "black",
+    fontSize: 48,
+    paddingBottom: 48,
+  },
+  pinViewContainer: {
+    marginTop: 24,
+  },
+  inputViewEmptyStyle: {
+    borderWidth: 1,
+    borderColor: "black",
+    backgroundColor: "white",
+    margin: 12,
+  },
+  inputViewFilledStyle: {
+    backgroundColor: "black",
+  },
+  buttonViewStyle: {
+    borderWidth: 1,
+    borderColor: "black",
+  },
+  buttonTextStyle: {
+    color: "black",
+  },
+});
 
 const darkstyles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#1c1c1c',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: "100%",
-      },
-      pinText: {
-        color: 'white',
-        fontSize: 48,
-        paddingBottom: 48,
-      },
-      pinViewContainer: {
-        marginTop: 24,
-      },
-      inputViewEmptyStyle:{
-        borderWidth: 1, 
-        borderColor: 'white', 
-        backgroundColor: '#1c1c1c',
-        margin:12   
-    },
-    inputViewFilledStyle:{
-        backgroundColor: 'white', 
-    },
-    buttonViewStyle:{
-        borderWidth: 1, 
-        borderColor: 'white',
-    },
-    buttonTextStyle:{
-        color: 'white',
-    }
-  });
-
-
+  container: {
+    flex: 1,
+    backgroundColor: "#1c1c1c",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+  },
+  pinText: {
+    color: "white",
+    fontSize: 48,
+    paddingBottom: 48,
+  },
+  pinViewContainer: {
+    marginTop: 24,
+  },
+  inputViewEmptyStyle: {
+    borderWidth: 1,
+    borderColor: "white",
+    backgroundColor: "#1c1c1c",
+    margin: 12,
+  },
+  inputViewFilledStyle: {
+    backgroundColor: "white",
+  },
+  buttonViewStyle: {
+    borderWidth: 1,
+    borderColor: "white",
+  },
+  buttonTextStyle: {
+    color: "white",
+  },
+});
 
 export default Pin;
--- MyApp/app/tabs/QandA.jsx
+++ MyApp/app/tabs/QandA.jsx
@@ -1,17 +1,24 @@
-import React, { useState , useEffect , useContext} from 'react'
-import { SettingContext } from '../system/setting';
+import React, { useState, useEffect, useContext } from "react";
+import { SettingContext } from "../system/setting";
 
-import { View, Button ,StyleSheet, Image, Text, ScrollView,} from 'react-native';
-import { data } from '../system/fetchData';
-import { db } from '../system/db';
-import { useNavigation } from '@react-navigation/native';
+import {
+  View,
+  Button,
+  StyleSheet,
+  Image,
+  Text,
+  ScrollView,
+} from "react-native";
+import { data } from "../system/fetchData";
+import { db } from "../system/db";
+import { useNavigation } from "@react-navigation/native";
 
 const QandA = () => {
   const { setting } = useContext(SettingContext);
   const { theme, language } = setting;
-  const styles = theme === 'light' ? lightstyles : darkstyles;
+  const styles = theme === "light" ? lightstyles : darkstyles;
   const [questions, setQuestions] = useState(() => {
-    if (language === 'EN') {
+    if (language === "EN") {
       return [
         {
           question: "LoremxD",
@@ -33,58 +40,62 @@ const QandA = () => {
           answer:
             "Aenean est quam, auctor vel rutrum ac, convallis eget ipsum. Nullam a venenatis purus, a dictum diam. Aliquam malesuada rutrum enim vitae maximus. In vitae mattis erat.",
         },
-        // Add more Q&A here
-      ];
-    } else {
-      return [
         {
           question: "เทสๆ",
           answer: "ตอบๆเว้ย",
-        }
+        },
+        // Add more Q&A here
       ];
     }
   });
-  const [sw , setSw] = useState(true);
+  const [sw, setSw] = useState(true);
   const [reloadKey, setReloadKey] = useState(0);
 
   useEffect(() => {
-    setReloadKey(prevKey => prevKey + 1);
-  }, [sw , navigation]);
+    setReloadKey((prevKey) => prevKey + 1);
+  }, [sw, navigation]);
 
   const handleSw = (bool) => {
     setSw(bool);
-    setReloadKey(prevKey => prevKey + 1);
-  }
+    setReloadKey((prevKey) => prevKey + 1);
+  };
 
   const user = db.getCurrentUser();
   const navigation = useNavigation();
-             
-  const qa_iconImage = theme === 'light' ? require('../picture/qa_icon.png') : require('../picture/qa_icon_w.png');
-  const qaImage = theme === 'light' ? require('../picture/qa.png') : require('../picture/qa_w.png');
-  
+
+  const qa_iconImage =
+    theme === "light"
+      ? require("../picture/qa_icon.png")
+      : require("../picture/qa_icon_w.png");
+  const qaImage =
+    theme === "light"
+      ? require("../picture/qa.png")
+      : require("../picture/qa_w.png");
+
   if (language == "EN") {
     return (
       <View style={styles.container}>
         <View style={styles.nav}>
           <View style={styles.inNav}>
             <Image style={[styles.logo]} source={qa_iconImage} />
-            <Text style={[styles.text,{marginLeft: 5, fontSize: 25,}]}>Q & A</Text>
+            <Text style={[styles.text, { marginLeft: 5, fontSize: 25 }]}>
+              Q & A
+            </Text>
           </View>
         </View>
-        
-        <View style={styles.containerContent} >
-          <View style={styles.boxx}>
-            <View style={styles.nav2}>
-              <Image style={styles.logo2} source={ qaImage }/>
-              <View style={{left: '20%'}}>
-                <Text style={[styles.text]}>Contact Admin</Text>
-                <Text style={[styles.text]}>Tel: 00000000</Text>
-                <Text style={[styles.text]}>Line: BBBBBB</Text>
-              </View>
+
+        <View style={styles.boxx}>
+          <View style={styles.contactAdminContainer}>
+            <Image style={styles.logo2} source={qaImage} />
+            <View style={styles.contactAdminInfo}>
+              <Text style={styles.contactAdminTitle}>Contact Admin</Text>
+              <Text style={styles.contactAdminText}>Tel: 00000000</Text>
+              <Text style={styles.contactAdminText}>Line: BBBBBB</Text>
             </View>
           </View>
         </View>
-        <ScrollView style={[styles.center,{marginLeft:0}]}>
+
+        <ScrollView style={[styles.center, { marginLeft: 0 }]}>
           {questions.map((item, index) => (
             <View key={index} style={styles.qa}>
               <Text style={styles.text2}>Q:{item.question}</Text>
@@ -94,34 +105,34 @@ const QandA = () => {
         </ScrollView>
       </View>
     );
-  }
-  else{
+  } else {
     return (
       <View style={styles.container}>
         <View style={styles.nav}>
           <View style={styles.inNav}>
             <Image style={[styles.logo]} source={qa_iconImage} />
-            <Text style={[styles.text,{marginLeft: 5, fontSize: 25,}]}>ช่วยเหลือ</Text>
+            <Text style={[styles.text, { marginLeft: 5, fontSize: 25 }]}>
+              ช่วยเหลือ
+            </Text>
           </View>
         </View>
-        
-        <View style={styles.containerContent} >
-          <View style={styles.boxx}>
-            <View style={styles.nav2}>
-              <Image style={styles.logo2} source={ qaImage }/>
-              <View style={{left: '20%'}}>
-                <Text style={[styles.text]}>ติดต่อ ผู้ดูแล</Text>
-                <Text style={[styles.text]}>โทร: 00000000</Text>
-                <Text style={[styles.text]}>ไลน์: BBBBBB</Text>
-              </View>
+
+        <View style={styles.boxx}>
+          <View style={styles.contactAdminContainer}>
+            <Image style={styles.logo2} source={qaImage} />
+            <View style={styles.contactAdminInfo}>
+              <Text style={styles.contactAdminTitle}>ติดต่อ แอดมิน</Text>
+              <Text style={styles.contactAdminText}>โทรศัพท์: 00000000</Text>
+              <Text style={styles.contactAdminText}>ไลน์: BBBBBB</Text>
             </View>
           </View>
         </View>
-        <ScrollView style={[styles.center,{marginLeft:0}]}>
+
+        <ScrollView style={[styles.center, { marginLeft: 0 }]}>
           {questions.map((item, index) => (
             <View key={index} style={styles.qa}>
-              <Text style={styles.text2}>Q:{item.question}</Text>
-              <Text style={styles.text2}>A:{item.answer}</Text>
+              <Text style={styles.text}>Q: {item.question}</Text>
+              <Text style={styles.text2}>A: {item.answer}</Text>
             </View>
           ))}
         </ScrollView>
@@ -130,83 +141,102 @@ const QandA = () => {
   }
 };
 
-
 const lightstyles = StyleSheet.create({
-    containerContent: {
-      marginBottom: 10,
-    },
-    container: {
-      backgroundColor: "#FFF",
-      height: "100%",
-    },
-    nav: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      marginTop: "10%",
-    },
-    nav2: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      marginTop: 0,
-      left: "-60%",
-    },
-    inNav: {
-      flexDirection: "row",
-      justifyContent: "flex-start",
-      alignItems: "center",
-      marginHorizontal: 5,
-      marginTop: 5,
-    },
-    text:{
-      color: 'black',
-      fontSize: 16,
-    },
-    text2:{
-      color: 'black',
-      fontSize: 16,
-      marginLeft:'5%',
-      marginRight:'5%',
-    },
-    center: {
-      left: "auto",
-      width: "80%",
-      left: "10%" ,
-      borderRadius: 20,
-    },
-    boxx: {
-      justifyContent: "flex-start",
-      alignItems: "center",
-      height: 'auto',
-      padding: 10,
-      marginTop: 20,
-      margin: 0,
-      color: 'white',
-      maxWidth: "80%",
-      left: "10%" ,
-      backgroundColor: "#ECECEC",
-      borderRadius: 20,
-    },
-    logo: {
-      width: 40,
-      height: 40,
-      marginTop: 0,
-      borderColor: 'white',
-    },
-    logo2: {
-      width: 100,
-      height: 100,
-      borderRadius: 30,
-    },
-    qa: {
-      marginBottom: 20,
-      borderWidth: 1,
-      borderColor: "#ddd",
-      padding: 10,
-      borderRadius: 20,
-    },
-  });
+  containerContent: {
+    marginBottom: 10,
+  },
+  container: {
+    backgroundColor: "#FFF",
+    height: "100%",
+  },
+  nav: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: "10%",
+  },
+  nav2: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 0,
+    left: "-60%",
+  },
+  inNav: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    marginHorizontal: 5,
+    marginTop: 5,
+  },
+  text: {
+    color: "black",
+    fontSize: 18,
+    marginLeft: "5%",
+    marginRight: "5%",
+  },
+  text2: {
+    color: "black",
+    fontSize: 16,
+    marginLeft: "5%",
+    marginRight: "5%",
+  },
+  center: {
+    left: "auto",
+    width: "80%",
+    left: "10%",
+    borderRadius: 20,
+  },
+  boxx: {
+    justifyContent: "flex-start",
+    alignItems: "center",
+    height: "auto",
+    padding: 10,
+    marginTop: 20,
+    margin: 0,
+    color: "black",
+    maxWidth: "80%",
+    left: "10%",
+    backgroundColor: "#ECECEC",
+    borderRadius: 20,
+    marginBottom: 15,
+  },
+  logo: {
+    width: 50,
+    height: 50,
+    marginTop: 0,
+    borderColor: "white",
+  },
+  logo2: {
+    width: 100,
+    height: 100,
+    borderRadius: 30,
+  },
+  qa: {
+    marginBottom: 20,
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 10,
+    borderRadius: 20,
+  },
+  contactAdminContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  contactAdminInfo: {
+    marginLeft: 20,
+  },
+  contactAdminTitle: {
+    color: "black",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  contactAdminText: {
+    color: "black",
+    fontSize: 16,
+    marginTop: 5,
+  },
+});
 
 const darkstyles = StyleSheet.create({
   containerContent: {
@@ -236,40 +266,43 @@ const darkstyles = StyleSheet.create({
     marginHorizontal: 5,
     marginTop: 5,
   },
-  text:{
-    color: 'white',
-    fontSize: 16,
+  text: {
+    color: "black",
+    fontSize: 18,
+    marginLeft: "5%",
+    marginRight: "5%",
   },
-  text2:{
-    color: 'white',
+  text2: {
+    color: "white",
     fontSize: 16,
-    marginLeft:'5%',
-    marginRight:'5%',
+    marginLeft: "5%",
+    marginRight: "5%",
   },
   center: {
     left: "auto",
     width: "80%",
-    left: "10%" ,
+    left: "10%",
     borderRadius: 20,
   },
   boxx: {
     justifyContent: "flex-start",
     alignItems: "center",
-    height: 'auto',
+    height: "auto",
     padding: 10,
     marginTop: 20,
     margin: 0,
-    color: 'white',
+    color: "white",
     maxWidth: "80%",
-    left: "10%" ,
+    left: "10%",
     backgroundColor: "#606060",
     borderRadius: 20,
+    marginBottom: 15,
   },
   logo: {
     width: 40,
     height: 40,
     marginTop: 0,
-    borderColor: 'white',
+    borderColor: "white",
   },
   logo2: {
     width: 100,
@@ -282,6 +315,23 @@ const darkstyles = StyleSheet.create({
     borderColor: "#ddd",
     padding: 10,
     borderRadius: 20,
+  },
+  contactAdminContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  contactAdminInfo: {
+    marginLeft: 20,
+  },
+  contactAdminTitle: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  contactAdminText: {
+    color: "white",
+    fontSize: 16,
+    marginTop: 5,
   },
 });
 
